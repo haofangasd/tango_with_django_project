c958faf1ecbb77adfdd9f87ecfad81f9f7dfda8a
Jie Chen <2271740c@student.gla.ac.uk>
2017-02-03 22:19:31 +0000

    chapter 5 Exercise

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,7 +6,7 @@ from rango.models import Page
 def index(request):
     context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context_dict)
